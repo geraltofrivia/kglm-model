e65b353b26ea7168a830263e1903f8a063fb4d00
Priyansh Trivedi <priyansh.trivedi@inria.fr>
2022-04-20 15:14:18 +0200

    Stripped away a lot of AllenNLP stuff from datareader. This will make the future process slower but if so we can trivially reactivate it

--- kglm/datareaders.py
+++ kglm/datareaders.py
@@ -61,27 +61,27 @@ class EnhancedWikitextKglmReader:
                                      "or 'discriminative'".format(mode))
         self._mode = mode
 
-        self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
-        self._entity_indexers = entity_indexers or {'entity_ids': SingleIdTokenIndexer(namespace='entity_ids')}
-        self._raw_entity_indexers = raw_entity_indexers or {
-            'raw_entity_ids': SingleIdTokenIndexer(namespace='raw_entity_ids')}
-        self._relation_indexers = relation_indexers or {'relations': SingleIdTokenIndexer(namespace='relations')}
-        if 'tokens' not in self._token_indexers or \
-                not isinstance(self._token_indexers['tokens'], SingleIdTokenIndexer):
-            raise ConfigurationError("EnhancedWikitextReader expects 'token_indexers' to contain "
-                                     "a 'single_id' token indexer called 'tokens'.")
-        if 'entity_ids' not in self._entity_indexers or \
-                not isinstance(self._entity_indexers['entity_ids'], SingleIdTokenIndexer):
-            raise ConfigurationError("EnhancedWikitextReader expects 'entity_indexers' to contain "
-                                     "a 'single_id' token indexer called 'entity_ids'.")
-        if 'raw_entity_ids' not in self._raw_entity_indexers or \
-                not isinstance(self._raw_entity_indexers['raw_entity_ids'], SingleIdTokenIndexer):
-            raise ConfigurationError("EnhancedWikitextReader expects 'raw_entity_indexers' to contain "
-                                     "a 'single_id' token indexer called 'raw_entity_ids'.")
-        if 'relations' not in self._relation_indexers or \
-                not isinstance(self._relation_indexers['relations'], SingleIdTokenIndexer):
-            raise ConfigurationError("EnhancedWikitextReader expects 'relation_indexers' to contain "
-                                     "a 'single_id' token indexer called 'relations'.")
+        # self._token_indexers = token_indexers or {'tokens': SingleIdTokenIndexer()}
+        # self._entity_indexers = entity_indexers or {'entity_ids': SingleIdTokenIndexer(namespace='entity_ids')}
+        # self._raw_entity_indexers = raw_entity_indexers or {
+        #     'raw_entity_ids': SingleIdTokenIndexer(namespace='raw_entity_ids')}
+        # self._relation_indexers = relation_indexers or {'relations': SingleIdTokenIndexer(namespace='relations')}
+        # if 'tokens' not in self._token_indexers or \
+        #         not isinstance(self._token_indexers['tokens'], SingleIdTokenIndexer):
+        #     raise ConfigurationError("EnhancedWikitextReader expects 'token_indexers' to contain "
+        #                              "a 'single_id' token indexer called 'tokens'.")
+        # if 'entity_ids' not in self._entity_indexers or \
+        #         not isinstance(self._entity_indexers['entity_ids'], SingleIdTokenIndexer):
+        #     raise ConfigurationError("EnhancedWikitextReader expects 'entity_indexers' to contain "
+        #                              "a 'single_id' token indexer called 'entity_ids'.")
+        # if 'raw_entity_ids' not in self._raw_entity_indexers or \
+        #         not isinstance(self._raw_entity_indexers['raw_entity_ids'], SingleIdTokenIndexer):
+        #     raise ConfigurationError("EnhancedWikitextReader expects 'raw_entity_indexers' to contain "
+        #                              "a 'single_id' token indexer called 'raw_entity_ids'.")
+        # if 'relations' not in self._relation_indexers or \
+        #         not isinstance(self._relation_indexers['relations'], SingleIdTokenIndexer):
+        #     raise ConfigurationError("EnhancedWikitextReader expects 'relation_indexers' to contain "
+        #                              "a 'single_id' token indexer called 'relations'.")
         self._alias_database = AliasDatabase.load(path=alias_database_path)
 
     # @overrides
@@ -172,68 +172,82 @@ class EnhancedWikitextKglmReader:
                         else:
                             mention_type[start + mode_offset] = 2
 
-                yield self.text_to_instance(source,
-                                            target,
-                                            shortlist,
-                                            reverse_shortlist,
-                                            raw_entity_ids,
-                                            entity_ids,
-                                            relations,
-                                            parent_ids,
-                                            shortlist_inds,
-                                            mention_type,
-                                            alias_copy_inds)
+                yield {
+                    "source": source,
+                    "target": target,
+                    "shortlist": shortlist,
+                    "reverse_shortlist": reverse_shortlist,
+                    "raw_entity_ids": raw_entity_ids,
+                    "entity_ids": entity_ids,
+                    "relations": relations,
+                    "parent_ids": parent_ids,
+                    "shortlist_inds": shortlist_inds,
+                    "mention_type": mention_type,
+                    "alias_copy_inds": alias_copy_inds
+                }
 
-    # noinspection PyTypeChecker
-    # @overrides
-    def text_to_instance(self,
-                         source,
-                         target=None,
-                         shortlist=None,
-                         reverse_shortlist=None,
-                         raw_entity_ids=None,
-                         entity_ids=None,
-                         relations=None,
-                         parent_ids=None,
-                         shortlist_inds=None,
-                         mention_type=None,
-                         alias_copy_inds=None) -> Instance:  # pylint: disable=arguments-differ
-        metadata = {
-            'source_tokens': source,
-            'alias_database': self._alias_database
-        }
-        fields = {
-            'metadata': MetadataField(metadata),
-            'source': TextField(tokenize(source), self._token_indexers),
-        }
+                # yield self.text_to_instance(source,
+                #                             target,
+                #                             shortlist,
+                #                             reverse_shortlist,
+                #                             raw_entity_ids,
+                #                             entity_ids,
+                #                             relations,
+                #                             parent_ids,
+                #                             shortlist_inds,
+                #                             mention_type,
+                #                             alias_copy_inds)
 
-        if target is not None:
-            fields['target'] = TextField(tokenize(target), self._token_indexers)
-            metadata['target_tokens'] = target
-        if shortlist is not None:
-            fields['shortlist'] = TextField(tokenize(shortlist), self._entity_indexers)
-        if raw_entity_ids is not None:
-            fields['raw_entity_ids'] = TextField(tokenize(raw_entity_ids), self._raw_entity_indexers)
-        if entity_ids is not None:
-            fields['entity_ids'] = TextField(tokenize(entity_ids), self._entity_indexers)
-        if parent_ids is not None:
-            fields['parent_ids'] = ListField([
-                TextField(tokenize(sublist),
-                          token_indexers=self._entity_indexers)
-                for sublist in parent_ids])
-        if relations is not None:
-            fields['relations'] = ListField([
-                TextField(tokenize(sublist),
-                          token_indexers=self._relation_indexers)
-                for sublist in relations])
-        if mention_type is not None:
-            fields['mention_type'] = SequentialArrayField(mention_type, dtype=np.int64)
-        if shortlist_inds is not None:
-            fields['shortlist_inds'] = SequentialArrayField(shortlist_inds, dtype=np.int64)
-        if alias_copy_inds is not None:
-            fields['alias_copy_inds'] = SequentialArrayField(alias_copy_inds, dtype=np.int64)
-
-        return Instance(fields)
+    # # noinspection PyTypeChecker
+    # # @overrides
+    # def text_to_instance(self,
+    #                      source,
+    #                      target=None,
+    #                      shortlist=None,
+    #                      reverse_shortlist=None,
+    #                      raw_entity_ids=None,
+    #                      entity_ids=None,
+    #                      relations=None,
+    #                      parent_ids=None,
+    #                      shortlist_inds=None,
+    #                      mention_type=None,
+    #                      alias_copy_inds=None) -> Instance:  # pylint: disable=arguments-differ
+    #     metadata = {
+    #         'source_tokens': source,
+    #         'alias_database': self._alias_database
+    #     }
+    #     fields = {
+    #         'metadata': MetadataField(metadata),
+    #         'source': TextField(tokenize(source), self._token_indexers),
+    #     }
+    #
+    #     if target is not None:
+    #         fields['target'] = TextField(tokenize(target), self._token_indexers)
+    #         metadata['target_tokens'] = target
+    #     if shortlist is not None:
+    #         fields['shortlist'] = TextField(tokenize(shortlist), self._entity_indexers)
+    #     if raw_entity_ids is not None:
+    #         fields['raw_entity_ids'] = TextField(tokenize(raw_entity_ids), self._raw_entity_indexers)
+    #     if entity_ids is not None:
+    #         fields['entity_ids'] = TextField(tokenize(entity_ids), self._entity_indexers)
+    #     if parent_ids is not None:
+    #         fields['parent_ids'] = ListField([
+    #             TextField(tokenize(sublist),
+    #                       token_indexers=self._entity_indexers)
+    #             for sublist in parent_ids])
+    #     if relations is not None:
+    #         fields['relations'] = ListField([
+    #             TextField(tokenize(sublist),
+    #                       token_indexers=self._relation_indexers)
+    #             for sublist in relations])
+    #     if mention_type is not None:
+    #         fields['mention_type'] = SequentialArrayField(mention_type, dtype=np.int64)
+    #     if shortlist_inds is not None:
+    #         fields['shortlist_inds'] = SequentialArrayField(shortlist_inds, dtype=np.int64)
+    #     if alias_copy_inds is not None:
+    #         fields['alias_copy_inds'] = SequentialArrayField(alias_copy_inds, dtype=np.int64)
+    #
+    #     return Instance(fields)
 
 
 if __name__ == '__main__':
